32bcd8c11e131873cb202c1f810e3ea1ce14bbae
cladendas <rvdagi05@yandex.ru>
2020-05-17 12:40:04 +0300

    Initial Commit

---
+++ Labyrinth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Labyrinth
+//
+//  Created by cladendas on 17.05.2020.
+//  Copyright © 2020 cladendas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
