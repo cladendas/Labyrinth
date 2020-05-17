1ca2edd65616178ee5bec00499878e64752ebbdf
cladendas <rvdagi05@yandex.ru>
2020-05-17 17:45:26 +0300

    Проработал выставление координат у комнат в зависимости от кол-ва комнат

--- Labyrinth/Controller/CreatorLabyrinth.swift
+++ Labyrinth/Controller/CreatorLabyrinth.swift
@@ -10,21 +10,51 @@ import Foundation
 
 class CreatorLabyrinth {
     
+    var rooms: [Room] = []
+    
     func build(numberOfRooms: Int) {
-        var Xmax: Int {
-            numberOfRooms / 2 - 1
+        
+        print(numberOfRooms / 2)
+        
+        guard numberOfRooms > 1 else {
+            return
         }
         
-        var Ymax: Int {
-            numberOfRooms % Xmax - 1
-        }
+        var count = 0
         
         var length: Int {
-            numberOfRooms / 2
+            return numberOfRooms / 2
         }
         
         var heigth: Int {
-            numberOfRooms / Xmax + numberOfRooms % Xmax
+            return numberOfRooms - length
         }
+        
+        var Xmax: Int {
+            return heigth - 1
+        }
+        
+        var Ymax: Int {
+            return length - 1
+        }
+        
+        var Xlast: Int {
+            return 0
+        }
+        
+        print(Xmax, Ymax, Xlast, length, heigth)
+        
+        for y in 0...Ymax {
+            for x in 0...Xmax {
+                if count != numberOfRooms {
+                    let room = Room()
+                    room.coordinate = [x, y]
+                    rooms.append(room)
+                    count += 1
+                    print(room.coordinate)
+                }
+            }
+        }
+        print("count", count)
     }
 }
